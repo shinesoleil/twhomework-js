2d2131a05b70134560ce899f32a1b9bf5008ca9a
shinesoleil <soleilpan@gmail.com>
2015-11-20 15:20:40 +0800

    remove repeat code; improve variable and function name

--- src/bookmarks.js
+++ src/bookmarks.js
@@ -1,47 +1,40 @@
-var bookmarks;
-
-$.ajaxSettings.async = false;
 $.getJSON("./bookmarks.json", function(data) {
-    bookmarks = data;
-});
-
-function convertTime(milliseconds) {
-    var d = new Date();
-    d.setTime(milliseconds);
-    return d.getFullYear() + "-" + (d.getMonth()+1) + "-" + (d.getDate()+1);
-}
-
-//initiate the page
-bookmarks.forEach(function(element) {
-    $("#content")
-        .append('<div>' +
-        '<p class="phrase">' + element.title + '</p>' +
-        '<span class="timestamp">Created @ ' + convertTime(element.created + "000") +'</span>' +
-        '</div>' +
-        '<hr>');
-});
-
-//input event
-$("#input").bind("input propertychange",function() {
-    var inputValue = $(this).val();
-    var re = new RegExp(inputValue, "ig");
-
-    //keep only matched bookmarks
-    function filterByWord(obj) {
-        return re.test(obj.title);
+    var bookmarks = data;
+    
+    function convertTime(milliseconds) {
+        var d = new Date();
+        d.setTime(milliseconds);
+        return d.getFullYear() + "-" + (d.getMonth()+1) + "-" + (d.getDate()+1);
     }
 
-    //append matched bookmarks to the page
-    function appendToPage(obj) {
-        var highlightedText = obj.title.replace(re, '<span class="highlight">$&</span>');
+    function renderArticle(title, time) {
         $("#content")
             .append('<div>' +
-            '<p class="phrase">' + highlightedText + '</p>' +
-            '<span class="timestamp">Created @ ' + convertTime(obj.created) +'</span>' +
+            '<p class="phrase">' + title + '</p>' +
+            '<span class="timestamp">Created @ ' + time +'</span>' +
             '</div>' +
             '<hr>');
     }
 
-    $("#content").html("");  //clear all tags
-    bookmarks.filter(filterByWord).map(appendToPage);
+    bookmarks.forEach(function(element) {
+        renderArticle(element.title, convertTime(element.created + "000"));
+    });
+
+    //input event
+    $("#input").bind("input propertychange",function() {
+        var inputValue = $(this).val();
+        var regExp = new RegExp(inputValue, "ig");
+
+        function filterByWord(article) {
+            return regExp.test(article.title);
+        }
+
+        function appendToPage(article) {
+            var highlightedText = article.title.replace(regExp, '<span class="highlight">$&</span>');
+            renderArticle(highlightedText, convertTime(article.created));
+        }
+
+        $("#content").html(""); //clear all contents
+        bookmarks.filter(filterByWord).map(appendToPage);
+    });
 });
